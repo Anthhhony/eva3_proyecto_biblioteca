936e5ec4a59e5d198185d8202251031a13b1db45
Anthhhony <maldonadocampusanoanthy@gmail.com>
2024-11-18 00:33:29 -0300

    commit 15

--- eva2Project/eva2App/models.py
+++ eva2Project/eva2App/models.py
@@ -46,13 +46,16 @@ class Cliente(models.Model):
         validators=[RegexValidator(regex=r'^\d{1,2}\.\d{3}\.\d{3}-[0-9Kk]$', message='Inserte un rut válido (ejemplo: xx.xxx.xxx-x)' )]
     )
     nombre = models.CharField(max_length=30)
-    telefono = models.IntegerField(
+    telefono = models.CharField(
+        max_length=11,
         validators=[RegexValidator(regex=r'^\d{9,10}$', message='Ingrese un número de teléfono adecuado (ejemplo: +56912345678)')]
     )
-    edad = models.IntegerField(
+    edad = models.CharField(
+        max_length=3,
         validators=[RegexValidator(regex=r'^\d{1,3}$', message='No puede tener una edad con más de 3 dígitos.')]
     )
-    correo = models.EmailField(
+    correo = models.CharField(
+        max_length=50,
         validators=[EmailValidator('El correo ingresado no es válido, compruebe el ejemplo: correoejemplo@gmail.com')]
     )
     libros_comprados = models.ForeignKey(Libro, on_delete=models.CASCADE, related_name='clientes', null=True, blank=True)
