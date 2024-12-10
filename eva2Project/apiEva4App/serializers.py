1a24c88bc6ba6ab670a26259e50ffac6deaee17f
Alejandro González <andresg.vergara04@gmail.com>
2024-12-10 19:34:32 -0300

    ultimos cambios de apiEva4App
    Finalmente...

--- eva2Project/apiEva4App/serializers.py
+++ eva2Project/apiEva4App/serializers.py
@@ -3,18 +3,44 @@ from eva2App.models import Autor, Libro, Cliente  # Importa los modelos de la ap
 
 class AutorSerializer(serializers.HyperlinkedModelSerializer):
     url = serializers.HyperlinkedIdentityField(view_name='autor-detail')
+
     class Meta:
         model = Autor
-        fields = '__all__'
+        fields = ['id', 'nombre', 'apellido', 'correo', 'url']
 
 class LibroSerializer(serializers.ModelSerializer):
     url = serializers.HyperlinkedIdentityField(view_name='libro-detail')
     class Meta:
         model = Libro
         fields = '__all__'
+        
+    def to_representation(self, instance):
+    # Llamamos a la representación original
+        representation = super().to_representation(instance)
+
+    # Extraemos el campo 'url' y lo removemos de la representación
+        url = representation.pop('url', None)
+    
+    # Agregamos el campo 'url' al final de la representación
+        if url:
+            representation['url'] = url
+    
+        return representation
 
 class ClienteSerializer(serializers.ModelSerializer):
     url = serializers.HyperlinkedIdentityField(view_name='cliente-detail')
     class Meta:
         model = Cliente
         fields = '__all__'
+    def to_representation(self, instance):
+        # Llamamos a la representación original
+            representation = super().to_representation(instance)
+        
+        # Extraemos el campo 'url' y lo removemos de la representación
+            url = representation.pop('url', None)
+        
+        # Agregamos el campo 'url' al final de la representación
+            if url:
+                representation['url'] = url
+        
+            return representation
